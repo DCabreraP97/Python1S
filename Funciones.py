ec2437f2ab5dd71a7d62cae0656a33746ba8b67b
Vina del Mar <vina@duoc.acad>
2024-06-17 22:31:21 -0400

    update

--- Funciones.py
+++ Funciones.py
@@ -1,6 +1,7 @@
 sector = ["norte","centro","sur"]
 pedidos = []
 
+
 def registrar_pedido(pedidos):
     gas_5 = 0
     gas_15 = 0
@@ -28,14 +29,9 @@ def registrar_pedido(pedidos):
                 gas_15 = gas_15 + 1
             if selec_gas == 3:
                 gas_45 = gas_45 + 1
-            opc_gas = input("Desea agregar otro gas? (si/no)").lower
-            if opc_gas == "si":
-                selec_gas = int(input("Por favor escoga el tipo de gas que desee: "))
-            else:
-                break
+            opc_gas = int(input("Desea otro? (1 = si/ 0 = no)"))
+
            
-
-
         pedidos.append({
             "Cliente" : nombre,
             "Direccion" : lugar,
@@ -50,17 +46,34 @@ def registrar_pedido(pedidos):
 
 
 def listar_pedido(pedidos):
-    print("Usted ha escogido listar todos los pedidos")
-    print(pedidos)
+    if pedidos == []:
+        print("Nada que mostrar, registre compras!")
+    else:
+        print("Usted ha escogido listar todos los pedidos")
+        print(pedidos)
 
 
 def imprimir_hoja_ruta(pedidos):
-    print("Usted ha escogido imprimir la hoja de ruta")
-    print("1. Sector Norte")
-    print("2. Sector Centro")
-    print("3. Sector Sur")
-    opc_hoja_ruta = int(input("Seleccione algun sector: "))
-    
-    #if opc_hoja_ruta == 1:
-     #   with open("Sector norte.txt","w") as archivo1:
-      #  contenido = pedidos[""]
+    if pedidos == []:
+        print("Nada que mostrar, registre compras!")
+    else:
+        print("Usted ha escogido imprimir la hoja de ruta")
+        print("1. Sector Norte")
+        print("2. Sector Centro")
+        print("3. Sector Sur")
+        opc_hoja_ruta = int(input("Seleccione algun sector: "))
+        
+        if opc_hoja_ruta == 1:
+            with open("Sector norte.txt","w") as archivo1:
+                contenido = pedidos[{"Sector"}]
+            print(contenido)
+
+        if opc_hoja_ruta == 2:
+            with open("Sector centro.txt","w") as archivo2:
+                contenido = pedidos["Sector","centro"]
+            print(contenido)
+
+        if opc_hoja_ruta == 3:
+            with open("Sector norte.txt","w") as archivo3:
+                contenido = pedidos["Sector","sur"]
+            print(contenido)
